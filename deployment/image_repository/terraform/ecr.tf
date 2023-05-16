3aa606678bd11385b276cd40d521e947cac59136
Clement Rey <clementrey-dev@gmail.com>
2023-05-16 16:22:35 +0200

    fix: missing pull through cache rule for quay

--- deployment/image_repository/terraform/ecr.tf
+++ deployment/image_repository/terraform/ecr.tf
@@ -26,6 +26,11 @@ resource "aws_ecr_pull_through_cache_rule" "ecr-public" {
   upstream_registry_url = "public.ecr.aws"
 }
 
+resource "aws_ecr_pull_through_cache_rule" "quay" {
+  ecr_repository_prefix = "quay"
+  upstream_registry_url = "quay.io"
+}
+
 resource null_resource "pull_python_env" {
   triggers = {
     always_run = timestamp()
