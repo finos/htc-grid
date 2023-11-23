083f799a7d66ae2afc72a6106dd98b8bc3c028c6
Flamur Gogolli <flamur.gogolli@gmail.com>
2023-11-23 15:09:01 +0000

    Remove Admin role from KMS Admins as it doesnt exist in WS

--- deployment/image_repository/terraform/main.tf
+++ deployment/image_repository/terraform/main.tf
@@ -12,8 +12,7 @@ locals {
 
   default_kms_key_admin_arns = [
     data.aws_caller_identity.current.arn,
-    "arn:${local.partition}:iam::${local.account_id}:root",
-    "arn:${local.partition}:iam::${local.account_id}:role/Admin"
+    "arn:${local.partition}:iam::${local.account_id}:root"
   ]
   additional_kms_key_admin_role_arns = [for k, v in data.aws_iam_role.additional_kms_key_admin_roles : v.arn]
   kms_key_admin_arns                 = concat(local.default_kms_key_admin_arns, local.additional_kms_key_admin_role_arns)
